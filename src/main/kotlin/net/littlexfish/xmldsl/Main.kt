c822c2edc42a2145dc8d80ef13f66ebd63e27d5d
Little Fish <i529561@gmail.com>
2024-05-27 11:47:07 +0800

    Fix argument parser not process correctly

--- src/main/kotlin/net/littlexfish/xmldsl/Main.kt
+++ src/main/kotlin/net/littlexfish/xmldsl/Main.kt
@@ -94,7 +94,7 @@ private fun argEquals(current: String, vararg arg: String): Boolean {
 			arg.any { it == current }
 		}
 		else {
-			sub.map { it.toString() }.any { "-$it" in arg }
+			sub.map { it }.any { "-$it" in arg }
 		}
 	}
 	if(current.startsWith("+")) {
@@ -103,7 +103,7 @@ private fun argEquals(current: String, vararg arg: String): Boolean {
 			arg.any { it == current }
 		}
 		else {
-			arg.map { it.substring(1) }.any { it == sub }
+			sub.map { it }.any { "+$it" in arg }
 		}
 	}
 	return false
@@ -172,7 +172,7 @@ private const val SETTINGS_INDENT = "indent"
 private const val SETTINGS_SHORTEN_EMPTY = "shorten-empty"
 private const val SETTINGS_ENV = "env"
 private const val SETTINGS_NOT_PARAM = "not-param"
-private const val SETTINGS_MODE = "mode" // keep to set mode in future
+private const val SETTINGS_MODE = "mode"
 
 private fun getDefaultSettings(): HashMap<String, Any?> {
 	return hashMapOf(
@@ -195,76 +195,72 @@ private fun parseArgs(args: Array<String>): HashMap<String, Any?>? {
 	var idx = 0
 	while(idx < args.size) {
 		val arg = args[idx]
-		when {
-			argEquals(arg, "-h", "--help") -> {
-				printHelp()
-				return null
+		if(argEquals(arg, "-h", "--help")) {
+			printHelp()
+			return null
+		}
+		if(argEquals(arg, "-v", "--version")) {
+			println(version)
+			return null
+		}
+		if(argEquals(arg, "-o", "--output")) {
+			settings[SETTINGS_OUTPUT] = getArgValue(args, arg, idx)
+			idx++
+		}
+		if(argEquals(arg, "--")) {
+			settings[SETTINGS_USER_INPUT] = true
+		}
+		if(argEquals(arg, "++")) {
+			settings[SETTINGS_TERMINAL_OUTPUT] = true
+		}
+		if(argEquals(arg, "-p", "--pretty-print")) {
+			settings[SETTINGS_PRETTY_PRINT] = true
+		}
+		if(argEquals(arg, "-s", "--shorten-empty")) {
+			settings[SETTINGS_SHORTEN_EMPTY] = true
+		}
+		if(argEquals(arg, "-I", "--input-charset")) {
+			val charset = getArgValue(args, arg, idx)
+			if(!Charset.isSupported(charset)) {
+				throw IllegalArgumentException("Charset $charset is not supported")
 			}
-			argEquals(arg, "-v", "--version") -> {
-				println(version)
-				return null
+			settings[SETTINGS_INPUT_CHARSET] = Charset.forName(charset)
+			idx++
+		}
+		if(argEquals(arg, "-O", "--output-charset")) {
+			val charset = getArgValue(args, arg, idx)
+			if(!Charset.isSupported(charset)) {
+				throw IllegalArgumentException("Charset $charset is not supported")
 			}
-			argEquals(arg, "-o", "--output") -> {
-				settings[SETTINGS_OUTPUT] = getArgValue(args, arg, idx)
-				idx++
+			settings[SETTINGS_OUTPUT_CHARSET] = Charset.forName(charset)
+			idx++
+		}
+		if(argEquals(arg, "-i", "--indent")) {
+			settings[SETTINGS_INDENT] = getArgValue(args, arg, idx)
+				.replace("\\t", "\t")
+				.replace("\\n", "\n")
+				.replace("\\r", "\r")
+				.replace("\\\\", "\\")
+			idx++
+		}
+		if(argEquals(arg, "-e", "--env")) {
+			val map = settings[SETTINGS_ENV] as? MutableMap<String, String> ?: mutableMapOf()
+			val kv = getArgValue(args, arg, idx)
+			val split = kv.split("=", limit = 2)
+			if(split.size != 2) {
+				throw IllegalArgumentException("Invalid environment variable $kv")
 			}
-			argEquals(arg, "--") -> {
-				settings[SETTINGS_USER_INPUT] = true
-			}
-			argEquals(arg, "++") -> {
-				settings[SETTINGS_TERMINAL_OUTPUT] = true
-			}
-			argEquals(arg, "-p", "--pretty-print") -> {
-				settings[SETTINGS_PRETTY_PRINT] = true
-			}
-			argEquals(arg, "-s", "--shorten-empty") -> {
-				settings[SETTINGS_SHORTEN_EMPTY] = true
-			}
-			argEquals(arg, "-I", "--input-charset") -> {
-				val charset = getArgValue(args, arg, idx)
-				if(!Charset.isSupported(charset)) {
-					throw IllegalArgumentException("Charset $charset is not supported")
-				}
-				settings[SETTINGS_INPUT_CHARSET] = Charset.forName(charset)
-				idx++
-			}
-			argEquals(arg, "-O", "--output-charset") -> {
-				val charset = getArgValue(args, arg, idx)
-				if(!Charset.isSupported(charset)) {
-					throw IllegalArgumentException("Charset $charset is not supported")
-				}
-				settings[SETTINGS_OUTPUT_CHARSET] = Charset.forName(charset)
-				idx++
-			}
-			argEquals(arg, "-i", "--indent") -> {
-				settings[SETTINGS_INDENT] = getArgValue(args, arg, idx)
-					.replace("\\t", "\t")
-					.replace("\\n", "\n")
-					.replace("\\r", "\r")
-					.replace("\\\\", "\\")
-				idx++
-			}
-			argEquals(arg, "-e", "--env") -> {
-				val map = settings[SETTINGS_ENV] as? MutableMap<String, String> ?: mutableMapOf()
-				val kv = getArgValue(args, arg, idx)
-				val split = kv.split("=", limit = 2)
-				if(split.size != 2) {
-					throw IllegalArgumentException("Invalid environment variable $kv")
-				}
-				map[split[0]] = split[1]
-				settings[SETTINGS_ENV] = map
-				idx++
-			}
-			argEquals(arg, "-m", "--mode") -> {
-				settings[SETTINGS_MODE] = getArgValue(args, arg, idx)
-				idx++
-			}
-			else -> {
-				if(!arg.startsWith("-")) {
-					val list = settings[SETTINGS_NOT_PARAM] as? MutableList<String> ?: mutableListOf()
-					list.add(arg)
-				}
-			}
+			map[split[0]] = split[1]
+			settings[SETTINGS_ENV] = map
+			idx++
+		}
+		if(argEquals(arg, "-m", "--mode")) {
+			settings[SETTINGS_MODE] = getArgValue(args, arg, idx)
+			idx++
+		}
+		if(!arg.startsWith("-")) {
+			val list = settings[SETTINGS_NOT_PARAM] as? MutableList<String> ?: mutableListOf()
+			list.add(arg)
 		}
 		idx++
 	}
